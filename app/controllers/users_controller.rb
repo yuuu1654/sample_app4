d88545ad7ee5ccb2aa72843ac6f7461f7bbbbf6c
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2021-04-25 20:46:56 +0900

    create user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+    
   end
+
+  #form_withの引数で必要となるUserオブジェクトを作成
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #保存成功の処理
+      flash[:success] = "ようこそサンプルウプにお越しくださいました！"
+      redirect_to @user #redirect_to user_url(@user)と同じ
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                    :password_confirmation)
+    end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  #ユーザー登録ボタンを押したときに(情報が無効な為に)、
+  #ユーザーが作成されない事を確認
+  #ユーザー数が（前と後で）同じというよりも、違わない事をテストする
+  
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "", 
+                                        email: "user@invalid", 
+                                        password: "foo", 
+                                        password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User", 
+                                        email: "user@example.com", 
+                                        password: "password", 
+                                        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
+end
