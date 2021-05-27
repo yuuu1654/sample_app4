40a78ee418f72e9350bfd42015dc1dbe446409a6
yuuu1654 <qgp36fg3qfelabaz@gmail.com>
2021-05-27 20:27:21 +0900

    create password_reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
 
   resources :users #多数の名前付きルート&アクションが利用できる
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
